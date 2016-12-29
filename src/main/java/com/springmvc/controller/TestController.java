2f530ddad0e7e0c9b70e21622adbb6391195ea70
yinfork <yinfork@foxmail.com>
2016-12-29 18:03:47 +0800

    [APK分析平台]完成文件上传功能

---
+++ src/main/java/com/springmvc/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class TestController {
+	@RequestMapping(value = "/test", method = RequestMethod.GET)
+	public String HelloWorld(Model model) {
+		model.addAttribute("message", "Hello Spring MVC!!!"); // 传参数给前端
+		return "test";
+	}
+}
--- src/main/java/com/springmvc/controller/UploadController.java
+++ src/main/java/com/springmvc/controller/UploadController.java
@@ -1,16 +1,45 @@
 package com.springmvc.controller;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
-
-@Controller  
+@Controller
 public class UploadController {
-	    @RequestMapping(value="/upload",method=RequestMethod.GET)  
-	    public String HelloWorld(Model model){ 
-	        model.addAttribute("message","Hello Spring MVC!!!");  //传参数给前端
-	        return "upload"; 
-	    }  
+	private final static String PATH = "//Users//yinjianhua//Desktop//hello";
+	
+	// 定位到上传单个文件界面
+	@RequestMapping(value = "/upload", method = RequestMethod.GET)
+	public String showUploadPage() {
+		return "uploadFile"; // view文件夹下的上传单个文件的页面
+	}
+
+	/**
+	 * 上传单个文件操作
+	 * 
+	 * @param multi
+	 * @return
+	 */
+	@RequestMapping(value = "/doUpload", method = RequestMethod.POST)
+	public String doUploadFile(@RequestParam("file") MultipartFile file) {
+
+		if (!file.isEmpty()) {
+			try {
+				FileUtils.copyInputStreamToFile(file.getInputStream(), new File(PATH,
+						System.currentTimeMillis() + file.getOriginalFilename()));
+
+			} catch (IOException e) {
+				e.printStackTrace();
+				System.out.println(e.toString());
+			}
+		}
+
+		return "success";
+	}
 }
